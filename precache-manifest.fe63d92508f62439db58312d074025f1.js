e0aa817162a0de9c5a8b1fdaaf6dbc7395ab098b
Kim Ngo <kngo@ms.innovativesol.com>
2020-03-04 15:26:46 -0500

    Updates

---
+++ precache-manifest.fe63d92508f62439db58312d074025f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d1f4189ffe4f9154b591136319239f",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/index.html"
+  },
+  {
+    "revision": "20e9ba0e546a01166208",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/static/css/main.4d924b0b.chunk.css"
+  },
+  {
+    "revision": "fe3c4ce391e4f6c40e52",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/static/js/2.68bfcd5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/static/js/2.68bfcd5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e9ba0e546a01166208",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/static/js/main.c32eeb48.chunk.js"
+  },
+  {
+    "revision": "abee6ebdd9b96eefc84f",
+    "url": "/kn4785/kn4785.github.io-react-tutorial.git/static/js/runtime-main.5a41c806.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kn4785/kn4785.github.io-react-tutorial.git/precache-manifest.fe63d92508f62439db58312d074025f1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kn4785/kn4785.github.io-react-tutorial.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
